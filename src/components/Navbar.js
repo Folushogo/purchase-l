a32d7019ddab4585d957c755b393659e00eca004
Folushogo <ewulotanfolusogo@gmail.com>
2024-08-29 09:06:06 +0100

    100%

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,60 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css'; 
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './Navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faBoxOpen, faInfoCircle, faWarehouse } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
+
+  // Determine the current page based on the route
+  const getPageTitle = () => {
+    switch (location.pathname) {
+      case '/items':
+        return { title: 'Items', icon: faBoxOpen };
+      case '/about':
+        return { title: 'About', icon: faInfoCircle };
+      case '/inventory':
+        return { title: 'Inventory', icon: faWarehouse };
+      default:
+        return { title: 'Home', icon: faHome };
+    }
+  };
+
+  const { title, icon } = getPageTitle();
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/items">Items</Link>
-      <Link to="/about">About</Link>
-      <Link to="/inventory">Inventory</Link>
+    <nav className={`navbar ${isOpen ? 'open' : ''}`}>
+      <button className="menu-icon" onClick={toggleNavbar}>
+        &#9776;
+      </button>
+      <div className="navbar-header">
+        <FontAwesomeIcon icon={icon} className="header-icon" />
+        <span className="navbar-title">{title}</span>
+      </div>
+      <div className={`navbar-links ${isOpen ? 'open' : ''}`}>
+        <Link to="/" onClick={() => setIsOpen(false)}>
+          <FontAwesomeIcon icon={faHome} className="mobile-icon" />
+          Home
+        </Link>
+        <Link to="/items" onClick={() => setIsOpen(false)}>
+          <FontAwesomeIcon icon={faBoxOpen} className="mobile-icon" />
+          Items
+        </Link>
+        <Link to="/about" onClick={() => setIsOpen(false)}>
+          <FontAwesomeIcon icon={faInfoCircle} className="mobile-icon" />
+          About
+        </Link>
+        <Link to="/inventory" onClick={() => setIsOpen(false)}>
+          <FontAwesomeIcon icon={faWarehouse} className="mobile-icon" />
+          Inventory
+        </Link>
+      </div>
     </nav>
   );
 };
